6300005e5c31c5330526535e7ab5e32d50361e3c
Matt Bellis <matthew.bellis@gmail.com>
2017-05-31 09:59:16 -0400

    Minor edits.

--- test/btagging.py
+++ test/btagging.py
@@ -10,15 +10,38 @@ tree = f.Get("TreeSemiLept")
 
 nentries = tree.GetEntries()
 btags = []
+jetpt = []
 for i in range(nentries):
     tree.GetEntry(i)
+
     jetbtag = tree.jetbtag
+    jpt = tree.jetpt
     njets = tree.njet
+
     for j in range(njets):
         btags.append(jetbtag[j])
-        print(jetbtag[j])
+        jetpt.append(jpt[j])
+        #print(jetbtag[j])
+
+btags = np.array(btags)
+jetpt = np.array(jetpt)
+        
 
 plt.figure()
 lch.hist_err(btags,bins=100,range=(0,1.25))
 plt.xlabel(r"b-tagging variable", fontsize = 18)
+
+plt.figure()
+lch.hist_err(btags[jetpt<30],bins=100,range=(0,1.25))
+plt.xlabel(r"b-tagging variable", fontsize = 18)
+
+plt.figure()
+lch.hist_err(btags[jetpt>=30],bins=100,range=(0,1.25))
+plt.xlabel(r"b-tagging variable", fontsize = 18)
+
+plt.figure()
+#plt.plot(btags[(btags>=0)*(jetpt<100)],jetpt[(btags>=0)*(jetpt<100)],'.',alpha=0.1)
+lch.hist_2D(btags[(btags>=0)*(jetpt<100)],jetpt[(btags>=0)*(jetpt<100)],xbins=100,ybins=100,xrange=(0,1),yrange=(0,100))
+plt.xlabel(r"b-tagging variable", fontsize = 18)
+
 plt.show()
