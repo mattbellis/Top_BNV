a95d0c3dcfa444541976553d5c60ee86bc31c91d
me25hage <me25hage@siena.edu>
2017-06-09 16:47:45 -0400

    final friday

--- test/electronMatching2.py
+++ test/electronMatching2.py
@@ -2,6 +2,7 @@ import ROOT, sys
 import numpy as np
 import matplotlib.pylab as plt
 import lichen.lichen as lch
+import math as m
 from PttoXYZ import PTtoXYZ
 
 def invmass(p4s):
@@ -15,6 +16,12 @@ def invmass(p4s):
     else:
         m = -np.sqrt(np.abs(m2))
     return m
+
+def XYZtoPT(x,y,z):
+    pt = x**2 + y**2
+    eta = -m.log(m.tan(m.atan(pt/z)/2.0))
+    phi = m.acos(x/pt)
+    return pt,eta,phi
 
 f = ROOT.TFile(sys.argv[1])
 tree = f.Get("TreeSemiLept")
@@ -206,8 +213,68 @@ for i in range(nentries):
                         #if top >= 160 and top <= 190:
                         topCandidatesH.append([jets[j1],jets[j2],b])
                         wCandidatesH.append([jets[j1],jets[j2]])
-	
+
+
+    # MC Truth Info
+    b = [0.,0.,0.,0.]
+    bb = [0.,0.,0.,0.]
+    wc1 = [0.,0.,0.,0.]
+    wc2 = [0.,0.,0.,0.]
+    wc1m = [0.,0.,0.,0.]
+    wc2m = [0.,0.,0.,0.]
+
+    b[0] = tree.gene[1]
+    b[1] = tree.genpt[1]
+    b[2] = tree.geneta[1]
+    b[3] = tree.genphi[1]
+    #b[1],b[2],b[3] = PTtoXYZ(b[1],b[2],b[3])
+
+    bb[0] = tree.gene[6]
+    bb[1] = tree.genpt[6]
+    bb[2] = tree.geneta[6]
+    bb[3] = tree.genphi[6]
+    #bb[1],bb[2],bb[3] = PTtoXYZ(bb[1],bb[2],bb[3])
+
+    wc1[0] = tree.gene[4]
+    wc1[1] = tree.genpt[4]
+    wc1[2] = tree.geneta[4]
+    wc1[3] = tree.genphi[4]
+    wc1pdg = tree.genpdg[4] 
+    #wc1[1],wc1[2],wc1[3] = PTtoXYZ(wc1[1],wc1[2],wc1[3])
     
+    wc2[0] = tree.gene[3]
+    wc2[1] = tree.genpt[3]
+    wc2[2] = tree.geneta[3]
+    wc2[3] = tree.genphi[3]
+    wc2pdg = tree.genpdg[3] 
+    #wc2[1],wc2[2],wc2[3] = PTtoXYZ(wc2[1],wc2[2],wc2[3])
+
+    wc1m[0] = tree.gene[9]
+    wc1m[1] = tree.genpt[9]
+    wc1m[2] = tree.geneta[9]
+    wc1m[3] = tree.genphi[9]
+    wc1mpdg = tree.genpdg[9] 
+    #wc1m[1],wc1m[2],wc1m[3] = PTtoXYZ(wc1m[1],wc1m[2],wc1m[3])
+    
+    wc2m[0] = tree.gene[8]
+    wc2m[1] = tree.genpt[8]
+    wc2m[2] = tree.geneta[8]
+    wc2m[3] = tree.genphi[8]
+    wc2mpdg = tree.genpdg[8] 
+    #wc2m[1],wc2m[2],wc2m[3] = PTtoXYZ(wc2m[1],wc2m[2],wc2m[3])
+   
+    if wc1pdg == 11 or wc1mpdg == -11 or wc1pdg == 11 or wc2pdg == -11:
+        for l in range(len(topCandidatesE)):
+            electron = topCandidatesE[l][1]
+            electrone, electronx, electrony, electronz = electron
+            electronpt, electroneta, electronphi = XYZtoPT(electronx,electrony,electronz)
+            if(abs(electronpt-wc1[1]) <= 1 or abs(electronpt - wc1m[1]) <= 1):
+                print('right electron')
+            if(abs(electronpt-wc2[1]) <= 1 or abs(electronpt - wc2m[1]) <= 1):
+                print('right electron')
+            
+
+
 Hmasses = []
 Mmasses = []
 Emasses = []
