5a8c6b70df46d9d58c7cd7446dcf1a70701d720b
mattbellis <matthew.bellis@gmail.com>
2017-07-03 14:41:31 -0500

    Minor changes.

--- test/slim_top_MC_cfg.py
+++ test/slim_top_MC_cfg.py
@@ -5,14 +5,15 @@ process = cms.Process("DAS")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000) )
 #process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
         fileNames = cms.untracked.vstring(
             #'root://cmseos.fnal.gov//store/user/cmsdas/2017/pre_exercises/DYJetsToLL.root'
             # This is a sample MC
-            'root://cmsxrootd.fnal.gov///store/mc/RunIISummer16MiniAODv2/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/18E31463-B3BE-E611-B6A3-0CC47A4D7678.root'
+            #'root://cmsxrootd.fnal.gov///store/mc/RunIISummer16MiniAODv2/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/18E31463-B3BE-E611-B6A3-0CC47A4D7678.root'
+            'root://cmsxrootd.fnal.gov////store/mc/RunIISummer16MiniAODv2/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/0693E0E7-97BE-E611-B32F-0CC47A78A3D8.root'
             # This is a sample data
             #'root://cmsxrootd.fnal.gov///store/data/Run2016B/SingleMuon/MINIAOD/23Sep2016-v3/00000/00AE0629-1F98-E611-921A-008CFA1112CC.root'
             #'file:///uscms/home/mbellis/eos_store/TTGJets_TuneCUETP8M1_13TeV-amcatnloFXFX-madspin-pythia8/crab_bellis_top_slim_MC/170112_192558/0000/top_slimmmed_files_MC_ttbar_1.root'
@@ -21,7 +22,7 @@ process.source = cms.Source("PoolSource",
 
 
 process.out = cms.OutputModule("PoolOutputModule",
-        fileName = cms.untracked.string('top_slimmmed_files_MC_ttbar.root'),
+        fileName = cms.untracked.string('top_slimmmed_files_MC_ttbar_NOTLOCAL_FOR_TESTING.root'),
         #fileName = cms.untracked.string('top_slimmmed_files_DATA_ttbar.root'),
         #fileName = cms.untracked.string('/uscms/home/mbellis/eos_store/TTGJets_TuneCUETP8M1_13TeV-amcatnloFXFX-madspin-pythia8/crab_bellis_top_slim_MC/170112_192558/0000/top_slimmmed_files_MC_ttbar_TESTLARGE.root'),
         #outputCommands = cms.untracked.vstring(['drop *', 'keep *_slimmedMuons__*', 'keep *_slimmedElectrons__*', 'keep *_slimmedJets__*', 'keep *_slimmedMETs__*'])
