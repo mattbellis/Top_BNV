fceb5738685f0e344c36013473c78d9db818f0c6
Madeline Hagen <me25hage@siena.edu>
2017-10-20 03:03:01 -0400

    topbnv_tools

--- sandbox/topbnv_tools.py
+++ sandbox/topbnv_tools.py
@@ -18,7 +18,19 @@ def invmass(E,px,py,pz):
     else:
         return -math.sqrt(-m2)
 
-    
+# Pass in an event and a tree and return gen particles
+def get_gen_particles(tree, event):
+	tree.GetEntry(event)
+	
+	jetE = tree.jet_energy
+	jetpx = tree.jet_px
+	jetpy = tree.jet_py
+	jetpz = tree.jet_pz
+
+	
+
+	 
+	   
     
 
 
