4b5529ed8c633f36d43751e961252f582d2aa189
Matt Bellis <matthew.bellis@gmail.com>
2019-02-11 11:23:15 -0500

    Added different stuff.

--- scratch/test_plot_pickle.py
+++ scratch/test_plot_pickle.py
@@ -6,10 +6,22 @@ import sys
 infile = open(sys.argv[1],'rb')
 data = pickle.load(infile)
 
+nbins = 100
+
 plt.figure()
 for i,key in enumerate(data.keys()):
     plt.subplot(4,4,i+1)
-    plt.hist(data[key],bins=50)
+    if key.find('had_j')>=0 and key.find('_m')>=0:
+        plt.hist(data[key],bins=nbins,range=(0,200))
+        plt.plot([83,83],[0,100],'--')
+    elif key.find('had_dR')>=0:
+        plt.hist(data[key],bins=nbins,range=(0,4))
+    elif key.find('had_dTheta')>=0:
+        plt.hist(data[key],bins=nbins,range=(0,3))
+    elif key.find('had_j')>=0 and key.find('_CSV')>=0:
+        plt.hist(data[key],bins=nbins,range=(0,1.1))
+    else:
+        plt.hist(data[key],bins=nbins)
     plt.title(key)
     print(key)
     print(data[key][0:5])
