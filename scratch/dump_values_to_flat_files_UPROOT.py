87b3ebeb692a3bf94725bb72342df7011caadba4
mattbellis <matthew.bellis@gmail.com>
2019-02-18 02:00:47 -0500

    Stuff.

--- scratch/dump_values_to_flat_files_UPROOT.py
+++ scratch/dump_values_to_flat_files_UPROOT.py
@@ -25,23 +25,23 @@ def main(infiles=None,outfilename=None):
     treename = "Tskim"
 
     leadmupt = []
-    topmass = []
+    hadtopmass = []
     Wmass = []
-    ntop = []
+    ncand = []
 
     leadmupt_cut0 = []
-    topmass_cut0 = []
+    hadtopmass_cut0 = []
     Wmass_cut0 = []
-    ntop_cut0 = []
+    ncand_cut0 = []
 
     leadmupt_cut1 = []
-    topmass_cut1 = []
+    hadtopmass_cut1 = []
     Wmass_cut1 = []
-    ntop_cut1 = []
+    ncand_cut1 = []
 
     jetcsv = []
     njet = []
-    nbjet = []
+    #nbjet = []
     nmuon = []
 
     for infile in infiles:
@@ -54,7 +54,7 @@ def main(infiles=None,outfilename=None):
         print(tree.keys())
         print(tree.array('nmuon'))
 
-        data = tree.arrays(["nmuon", "leadmupt", "ntop","topmass","nW","Wmass","nbjet","njet","jetcsv"])
+        data = tree.arrays(["nmuon", "leadmupt", "ncand","hadtopmass","Wmass","njet","jetcsv"])
                            
         print(type(data))
 
@@ -79,36 +79,36 @@ def main(infiles=None,outfilename=None):
             if lmupt>25:
                 leadmupt_cut0.append(lmupt)
 
-            ntop.append(data[b'ntop'][i])
-            for n in range(data[b'ntop'][i]):
-                topmass.append(data[b'topmass'][i][n])
+            ncand.append(data[b'ncand'][i])
+            for n in range(data[b'ncand'][i]):
+                hadtopmass.append(data[b'hadtopmass'][i][n])
                 if lmupt>25:
-                    topmass_cut0.append(data[b'topmass'][i][n])
+                    hadtopmass_cut0.append(data[b'hadtopmass'][i][n])
 
-            for n in range(data[b'nW'][i]):
+            for n in range(data[b'ncand'][i]):
                 wm = data[b'Wmass'][i][n]
                 Wmass.append(wm)
                 if lmupt>25:
                     Wmass_cut0.append(wm)
 
-            nbjet.append(data[b'nbjet'][i])
+            #nbjet.append(data[b'nbjet'][i])
 
             njet.append(data[b'njet'][i])
             for n in range(data[b'njet'][i]):
                 jetcsv.append(data[b'jetcsv'][i][n])
 
     leadmupt = np.array(leadmupt)
-    topmass = np.array(topmass)
+    hadtopmass = np.array(hadtopmass)
     Wmass = np.array(Wmass)
     jetcsv = np.array(jetcsv)
     njet = np.array(njet)
-    nbjet = np.array(nbjet)
-    ntop = np.array(ntop)
+    #nbjet = np.array(nbjet)
+    ncand = np.array(ncand)
     nmuon = np.array(nmuon)
 
 
     if outfilename == None:
-        outfilename = "/data/physics/bellis/CMS/HISTOGRAM_FILES/{0}_HISTOGRAMS.txt".format(infiles[0].split('/')[-1].split('.')[0])
+        outfilename = "/data/physics/bellis/CMS/HISTOGRAM_FILES_FEB2019/{0}_HISTOGRAMS.txt".format(infiles[0].split('/')[-1].split('.')[0])
     print(outfilename)
     #exit()
     outfile = open(outfilename,'w')
@@ -117,8 +117,8 @@ def main(infiles=None,outfilename=None):
     h,bin_edges = np.histogram(leadmupt,bins=400,range=(0,400))
     output += prepare_histogram_for_output("leadmupt",h,bin_edges)
 
-    h,bin_edges = np.histogram(topmass,bins=400,range=(0,800))
-    output += prepare_histogram_for_output("topmass",h,bin_edges)
+    h,bin_edges = np.histogram(hadtopmass,bins=400,range=(0,800))
+    output += prepare_histogram_for_output("hadtopmass",h,bin_edges)
 
     h,bin_edges = np.histogram(Wmass,bins=400,range=(0,800))
     output += prepare_histogram_for_output("Wmass",h,bin_edges)
@@ -129,8 +129,8 @@ def main(infiles=None,outfilename=None):
     h,bin_edges = np.histogram(leadmupt_cut0,bins=400,range=(0,400))
     output += prepare_histogram_for_output("leadmupt_cut0",h,bin_edges)
 
-    h,bin_edges = np.histogram(topmass_cut0,bins=400,range=(0,800))
-    output += prepare_histogram_for_output("topmass_cut0",h,bin_edges)
+    h,bin_edges = np.histogram(hadtopmass_cut0,bins=400,range=(0,800))
+    output += prepare_histogram_for_output("hadtopmass_cut0",h,bin_edges)
 
     h,bin_edges = np.histogram(Wmass_cut0,bins=400,range=(0,800))
     output += prepare_histogram_for_output("Wmass_cut0",h,bin_edges)
