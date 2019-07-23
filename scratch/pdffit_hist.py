aca10744be5e4200568b29658808602a409020d9
maddy <me25hage@titan>
2019-07-23 15:33:09 -0400

    pdf fit

--- scratch/pdffit_hist.py
+++ scratch/pdffit_hist.py
@@ -9,33 +9,76 @@ parser = OptionParser()
 import ROOT 
 
 def main(infiles=None):
-    f = ROOT.TFile.Open(infile)
-    Tree = f.Get(f.GetListOfKeys().At(0).GetName())
 
-    nentries = Tree.GetEntries()
+    c1 = ROOT.TCanvas("LeadMuPT","LeadMuPT",1000,1000)
+    c1.Divide(len(infiles))
 
-    leadmupt= ROOT.RooRealVar("leadmupt","leadmupt",0,1)
-    aset= ROOT.RooArgSet(leadmupt,"aset")
-    data= ROOT.RooDataSet("data","data",aset)
-
-    for n in range (nentries):
-        Tree.GetEntry()
-
-        leadmupt.setVal(Tree.leadmupt)
-        data.add(aset)
-
+    leadmupt= ROOT.RooRealVar("leadmupt","leadmupt",0,300)
     frame= leadmupt.frame()
-    data.plotOn(frame)
-    frame.Draw()
+
+    leg = ROOT.TLegend(0.8,.9,0.8,0.9)
+    leg.SetBorderSize(0)
+    leg.SetFillColor(0)
+    leg.SetFillStyle(0)
+    leg.SetTextFont(42)
+    #leg.SetTextSize(.015)
 
 
-## Wait for input to keep the GUI (which lives on a ROOT event dispatcher) alive
-if __name__ == '__main__':
-    infiles = sys.argv[1:]
-    main(infiles)
+    c = 0
+    for infile in infiles:
+        c1.cd(c)
+        c += 1
+
+        if "MC" in infile:
+            t = "MC"
+        else:
+            t = "Data"
+
+        f = ROOT.TFile.Open(infile)
+        Tree = f.Get(f.GetListOfKeys().At(0).GetName())
+
+        nentries = Tree.GetEntries()
+
+        aset= ROOT.RooArgSet(leadmupt,"aset")
+        data= ROOT.RooDataSet("data","data",aset)
+
+        x = []
+        print(nentries)
+        for n in range (nentries):
+            Tree.GetEntry(n)
+            x.append(getattr(Tree,"leadmupt"))
+        
+        for n in range(nentries):
+            leadmupt.setVal(x[n])
+            data.add(aset)
+
+        data.plotOn(frame, ROOT.RooFit.MarkerColor(c))
+
+        leg.AddEntry(data,t,"p")
+        '''
+        if c > 1:
+            frame.Draw("same")
+        else:
+            frame.Draw()
+
+        ROOT.gPad.Update()
+        '''
+        ROOT.gPad.SetLeftMargin(0.15)
+        frame.GetYaxis().SetTitleOffset(1.6)
+
+        frame.Draw()
+
+    leg.Draw()
+    
+
     rep = ''
     while not rep in [ 'q', 'Q' ]:
         rep = input( 'enter "q" to quit: ' )
         if 1 < len(rep):
           rep = rep[0]
 
+
+## Wait for input to keep the GUI (which lives on a ROOT event dispatcher) alive
+if __name__ == '__main__':
+    infiles = sys.argv[1:]
+    main(infiles)
