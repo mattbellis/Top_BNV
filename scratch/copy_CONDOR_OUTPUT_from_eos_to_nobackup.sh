e60a382b7542b654405edd7d52931a28d7f2dd1c
mattbellis <matthew.bellis@gmail.com>
2019-07-26 13:20:56 -0500

    Added copy file.

---
+++ scratch/copy_CONDOR_OUTPUT_from_eos_to_nobackup.sh
@@ -0,0 +1,2 @@
+xrdcp -f --recursive root://cmseos.fnal.gov//store/user/mbellis/CONDOR_output_files_2019/MC /uscms_data/d1/mbellis/CONDOR_output_files_2019/.
+#xrdcp --recursive root://cmseos.fnal.gov//store/user/mbellis/CONDOR_output_files_Feb2019/SingleElectron /uscms_data/d1/mbellis/CONDOR_output_files_Feb2019/SingleElectron/.
