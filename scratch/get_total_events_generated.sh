e8aae634726b3fe4412a4e04cf13888d89269703
mattbellis <matthew.bellis@gmail.com>
2020-05-29 01:57:03 -0500

    Simple

---
+++ scratch/get_total_events_generated.sh
@@ -0,0 +1,3 @@
+dasgoclient -query="file dataset=/ST_tW_antitop_5f_NoFullyHadronicDecays_13TeV-powheg_TuneCUETP8M1/RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3_ext1-v2/MINIAODSIM | sum(file.nevents)"
+
+dasgoclient -query="file dataset=/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3-v1/MINIAODSIM | sum(file.nevents)"
