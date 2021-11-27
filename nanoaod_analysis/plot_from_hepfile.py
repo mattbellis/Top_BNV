69a7bcd66bb77c4994d896ec659313c7068a8314
Matt Bellis <matthew.bellis@gmail.com>
2021-11-27 17:00:16 -0500

    Added some hepfile utility

---
+++ nanoaod_analysis/plot_from_hepfile.py
@@ -0,0 +1,87 @@
+import sys
+import numpy as np
+import matplotlib.pylab as plt
+
+import hepfile
+
+################################################################################
+def get_4vecs(data,idx):
+
+    e = data['jet/e'][idx]
+    px = data['jet/px'][idx]
+    py = data['jet/py'][idx]
+    pz = data['jet/pz'][idx]
+
+    return (e,px,py,pz)
+################################################################################
+
+################################################################################
+def invmass(p4s):
+
+    np4s = len(p4s)
+
+    e = p4s[0][0].copy()
+    px = p4s[0][1].copy()
+    py = p4s[0][2].copy()
+    pz = p4s[0][3].copy()
+
+    for i in range(1,np4s):
+        e += p4s[i][0]
+        px += p4s[i][1]
+        py += p4s[i][2]
+        pz += p4s[i][3]
+
+    m2 = e*e - (px*px + py*py + pz*pz)
+
+    m = m2
+
+    m[m2<0] = -np.sqrt(-m2[m2<0])
+    m[m2>0] = np.sqrt(m2[m2>0])
+
+    return m2
+################################################################################
+
+
+infilename = sys.argv[1]
+
+data,event = hepfile.load(infilename)
+
+nevents = hepfile.get_nbuckets_in_data(data)
+
+b1idx = np.arange(0,nevents*6,6)
+q11idx = np.arange(1,nevents*6,6)
+q12idx = np.arange(2,nevents*6,6)
+b2idx = np.arange(3,nevents*6,6)
+q21idx = np.arange(4,nevents*6,6)
+q22idx = np.arange(5,nevents*6,6)
+
+b1 = get_4vecs(data,b1idx)
+q11 = get_4vecs(data,q11idx)
+q12 = get_4vecs(data,q12idx)
+b2 = get_4vecs(data,b2idx)
+q21 = get_4vecs(data,q21idx)
+q22 = get_4vecs(data,q22idx)
+
+wpm = invmass([q11,q12])
+wmm = invmass([q21,q22])
+
+tm = invmass([b1,q11,q12])
+tbarm = invmass([b2,q21,q22])
+
+plt.figure()
+
+plt.subplot(2,2,1)
+plt.hist(wpm,bins=50,range=(0,300))
+
+plt.subplot(2,2,2)
+plt.hist(wmm,bins=50,range=(0,300))
+
+plt.subplot(2,2,3)
+plt.hist(tm,bins=50,range=(0,500))
+
+plt.subplot(2,2,4)
+plt.hist(tbarm,bins=50,range=(0,500))
+
+plt.savefig('image_from_h5.png')
+
+#plt.show()
--- nanoaod_analysis/test_genparticle_tracing.py
+++ nanoaod_analysis/test_genparticle_tracing.py
@@ -11,10 +11,20 @@ import nanoaod_analysis_tools as nat
 
 import time
 
+import hepfile 
+
 # https://coffeateam.github.io/coffea/api/coffea.nanoevents.methods.nanoaod.GenParticle.html
 
 # This might be helpful
 # https://github.com/CoffeaTeam/coffea/blob/master/binder/nanoevents.ipynb
+
+data = hepfile.initialize()
+hepfile.create_group(data,'jet',counter='njet')
+hepfile.create_dataset(data,['e','px','py','pz','btag'],group='jet',dtype=float)
+event = hepfile.create_single_bucket(data)
+
+
+
 
 infilename = sys.argv[1]
 
@@ -28,7 +38,7 @@ print("Reading in {0}".format(infilename))
 events = NanoEventsFactory.from_root(infilename, schemaclass=NanoAODSchema).events()
 print(len(events))
 
-#events = events[0:10]
+events = events[0:1000]
 
 jets = events.Jet
 
@@ -176,7 +186,13 @@ wpmass = []
 icount = 0
 for partons,jets_in_event in zip(quark_partons,jets):
 
-    if icount%100==0:
+    print("Before: ", len(jets_in_event))
+    print(jets_in_event.pt)
+    jets_in_event = jets_in_event[jets_in_event.pt>10]
+
+    print("After: ",len(jets_in_event))
+
+    if icount%10==0:
         print(icount)
 
     b1,b2 = None,None
@@ -247,6 +263,53 @@ for partons,jets_in_event in zip(quark_partons,jets):
         tm = nat.invmass([b2]+q2)
         tbarmass.append(tm)
 
+        # Hepfile stuff
+        event['jet/e'].append(b1[0])
+        event['jet/px'].append(b1[1])
+        event['jet/py'].append(b1[2])
+        event['jet/pz'].append(b1[3])
+        event['jet/btag'].append(-999)
+
+        event['jet/e'].append(q1[0][0])
+        event['jet/px'].append(q1[0][1])
+        event['jet/py'].append(q1[0][2])
+        event['jet/pz'].append(q1[0][3])
+        event['jet/btag'].append(-999)
+
+        event['jet/e'].append(q1[1][0])
+        event['jet/px'].append(q1[1][1])
+        event['jet/py'].append(q1[1][2])
+        event['jet/pz'].append(q1[1][3])
+        event['jet/btag'].append(-999)
+
+        event['jet/e'].append(b2[0])
+        event['jet/px'].append(b2[1])
+        event['jet/py'].append(b2[2])
+        event['jet/pz'].append(b2[3])
+        event['jet/btag'].append(-999)
+
+        event['jet/e'].append(q2[0][0])
+        event['jet/px'].append(q2[0][1])
+        event['jet/py'].append(q2[0][2])
+        event['jet/pz'].append(q2[0][3])
+        event['jet/btag'].append(-999)
+
+        event['jet/e'].append(q2[1][0])
+        event['jet/px'].append(q2[1][1])
+        event['jet/py'].append(q2[1][2])
+        event['jet/pz'].append(q2[1][3])
+        event['jet/btag'].append(-999)
+        print("--------")
+        print(b1)
+        print(q1)
+        print(b2)
+        print(q2)
+
+        #return_value = hepfile.pack(data,event,STRICT_CHECKING=True)
+        return_value = hepfile.pack(data,event)
+        if return_value != 0:
+            exit()
+
     icount += 1
 
 print(f"Matched partons: {nmatched_partons}")
@@ -257,17 +320,24 @@ print(f"Matched events:  {nmatched_events}")
 
 plt.figure()
 plt.subplot(2,2,1)
-plt.hist(wpmass,bins=50,range=(0,150))
+plt.hist(wpmass,bins=50,range=(0,300))
 
 plt.subplot(2,2,2)
-plt.hist(wmmass,bins=50,range=(0,150))
+plt.hist(wmmass,bins=50,range=(0,300))
 
 plt.subplot(2,2,3)
-plt.hist(tmass,bins=50,range=(0,300))
+plt.hist(tmass,bins=50,range=(0,500))
 
 plt.subplot(2,2,4)
-plt.hist(tbarmass,bins=50,range=(0,300))
+plt.hist(tbarmass,bins=50,range=(0,500))
+
+plt.savefig('image.png')
 
 0
 
-plt.show()
+outfilename = f'{infilename.split("/")[-1].split(".root")[0]}.h5'
+print(f"Writing to {outfilename}")
+hdfile = hepfile.write_to_file(outfilename,data,comp_type='gzip',comp_opts=9,verbose=True)
+
+
+#plt.show()
